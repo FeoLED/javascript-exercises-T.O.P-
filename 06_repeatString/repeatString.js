67707e163c7ac291bbd09477a72b05ec8a5841c0
FeoLED <naranjasdelespacio@gmail.com>
2025-07-01 18:58:31 -0300

    made function with error handling for negative value

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = "";
+    if(num<0){
+        repeated = "ERROR";
+    }else{
+        for(let i=0; i<num; i++){
+        repeated = repeated + string;
+        }
+    }
+    return repeated;
 };
 
 // Do not edit below this line

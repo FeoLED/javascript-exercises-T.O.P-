5f8e09559908ed317ed27f15df79f8862e9fce6e
FeoLED <naranjasdelespacio@gmail.com>
2025-07-02 19:06:43 -0300

    added function to return a reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(oracion) {
+    let separado = oracion.split("");
+    let invertido = separado.reverse();
+    let oracionInversa = invertido.join("");
+    return oracionInversa
 };
 
 // Do not edit below this line

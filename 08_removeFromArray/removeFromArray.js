eb09ab7d386a3c19983b5a025b46781cd4de5236
FeoLED <naranjasdelespacio@gmail.com>
2025-07-03 06:43:36 -0300

    made the function with resting aruguments

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...victim){
+    let modifiedArr = [];
+    for(index in arr){
+        if(!victim.includes(arr[index])){
+            modifiedArr.push(arr[index]);
+        }
+    }
+    return modifiedArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

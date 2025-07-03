c707d85570f78bd709bba8e79a2add185e75dad7
FeoLED <naranjasdelespacio@gmail.com>
2025-07-03 13:21:09 -0300

    added function with proper error handling

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let contador = 0;
+    if(typeof(a)!="number"||typeof(b)!="number"||!Number.isInteger(a)||!Number.isInteger || a<0 || b<0){
+        return "ERROR";
+    }else if(a>b){
+        for(let i = b; i<(a+1); i++){
+            contador = contador + i;
+        }
+        return contador;
+    }else{
+        for(let i = a; i<(b+1); i++){
+            contador = contador + i;
+        }
+        return contador;
+    }
+   
 };
 
 // Do not edit below this line

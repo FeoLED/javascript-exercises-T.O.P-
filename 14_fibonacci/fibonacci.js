79bdd88b51c9f85cb1f3b3b97ab508df20548a57
FeoLED <naranjasdelespacio@gmail.com>
2025-07-23 16:40:54 -0300

    added fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    parseInt(n);
+    if(n<0){
+        return "OOPS";
+    }else if(n==0){
+        return 0
+    }else if(n==1){
+        return 1
+    }else{
+        let firstNum = 1;
+        let secondNum = 0;
 
+        for (let i = 2; i <= n; i++) {
+            let actual = firstNum + secondNum;
+            secondNum = firstNum;
+            firstNum = actual;
+        }
+
+        return firstNum;
+    }
 };
 
 // Do not edit below this line

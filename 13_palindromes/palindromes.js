90320d07ae9dd224f91b6840669ce32f31c59e0c
FeoLED <naranjasdelespacio@gmail.com>
2025-07-23 15:32:46 -0300

    added function to solve palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let abc = "abcdefghijklmnñopqrstuvwxyz0123456789";
+    let newString = "";
+    for(let i in string){
+        for(let j in abc){
+            if((string[i]==abc[j])){
+                newString = newString + string[i];
+            }
+        }
+    }
+    let temporaryVessel = newString.split("").reverse().join("");
+    return (temporaryVessel==newString);
 };
 
 // Do not edit below this line

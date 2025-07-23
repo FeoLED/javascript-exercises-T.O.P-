0ba8f42fb8a3afad849bca63ce8837399b80c73a
FeoLED <naranjasdelespacio@gmail.com>
2025-07-23 18:41:07 -0300

    made function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+function edad (fechaNacio, fechaMurio){
+    if(!fechaMurio){
+        fechaMurio = new Date().getFullYear();
+    }
+    return fechaMurio - fechaNacio;
+};
+ 
+const findTheOldest = function(gente) {
+return gente.reduce((mayor, actual)=>{
+    let mayorEdad = edad(mayor.yearOfBirth, mayor.yearOfDeath);
+    let edadActual = edad(actual.yearOfBirth, actual.yearOfDeath);
+        if(mayorEdad>edadActual){
+            return mayor;
+        }else{
+            return actual;
+        }
+    });
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
